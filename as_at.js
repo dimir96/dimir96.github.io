43e80fc98a09c2881eb0b31990c6336ff9d119ac
dimir96 <58175832+dimir96@users.noreply.github.com>
2025-01-26 23:24:55 +0100

    Update as_at.js

--- as_at.js
+++ as_at.js
@@ -3,9 +3,9 @@
 
 	//Не нужно переписывать весь этот код, не делайте так!
 	//for Lampa.
-	window.lampa_settings.torrents_use = true;
-	window.lampa_settings.demo = false;
-	window.lampa_settings.read_only = false;
+	//window.lampa_settings.torrents_use = true;
+	//window.lampa_settings.demo = false;
+	//window.lampa_settings.read_only = false;
 
 
 	const canvas = document.createElement('canvas');
