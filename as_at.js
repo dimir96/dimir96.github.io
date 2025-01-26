aeb29bde2e37802fa6505d40406fe4e4fc9b4e30
dimir96 <58175832+dimir96@users.noreply.github.com>
2025-01-26 22:52:18 +0100

    Create as_at.js

---
+++ as_at.js
@@ -0,0 +1,14 @@
+(function () {
+	'use strict';
+
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 200;
+    canvas.height = 200;
+    document.body.appendChild(canvas);
+    
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 200, 200);
+
+})();
