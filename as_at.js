c12cba05944c97d55acbd7988b4f1e56f42855bf
dimir96 <58175832+dimir96@users.noreply.github.com>
2025-01-26 23:06:22 +0100

    Update as_at.js

--- as_at.js
+++ as_at.js
@@ -7,4 +7,14 @@
 	window.lampa_settings.demo = false;
 	window.lampa_settings.read_only = false;
 
+
+	const canvas = document.createElement('canvas');
+    	canvas.width = 200;
+    	canvas.height = 200;
+    	document.body.appendChild(canvas);
+    
+    	const ctx = canvas.getContext('2d');
+    	ctx.fillStyle = 'blue';
+    	ctx.fillRect(0, 0, 200, 200);
+
 })();
