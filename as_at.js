87da269a0f6f71ebc8b0143bc0103f5a56cc952c
dimir96 <dimir996@gmail.com>
2025-02-01 23:25:03 +0100

    sdf

--- as_at.js
+++ as_at.js
@@ -27,10 +27,11 @@
 						var e = a.logos[0].file_path;
 						if (e != "") {
 							$(".full-start-new__title").html(
-								'<img style="margin-top: 5px;max-height: 300px; object-fit: contain;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />'
+								'<img style="margin-top: 5px;max-height: 300px; max-width: 400px;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />'
 							);
 						}
 					}
+
 				});
 			}
 		});
