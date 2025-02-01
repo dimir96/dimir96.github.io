93b242fd37d05d9e8ea081586f084fca02579046
dimir96 <dimir996@gmail.com>
2025-02-01 22:53:03 +0100

    test dimention

--- as_at.js
+++ as_at.js
@@ -27,7 +27,7 @@
 						var e = a.logos[0].file_path;
 						if (e != "") {
 							$(".full-start-new__title").html(
-								'<img style="margin-top: 5px;max-height: 300px;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />'
+								'<img style="margin-top: 5px;min-height: 200px; max-width: 50%;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />'
 							);
 						}
 					}
